b9fe0cb88dc1f24ab89e6b38dedca506b472dcf0
NIk Chernyh <nik@arkadia.com>
2013-05-07 10:59:50 +0400

    update

--- jquery.bkmenu.js
+++ jquery.bkmenu.js
@@ -28,7 +28,8 @@
       'afterShow'            : function(){},
       'beforeHide'           : function(){},
       'afterHide'            : function(){},
-			'showContentTiming'    : 10
+      'showContentTiming'    : 100,
+      'hideMenuTiming'       : 600,
     }, options);
 		
 		var init = function(){
@@ -48,25 +49,29 @@
 					if (menuMouseover) return false;
 				  menu.find('li').removeClass(settings.classSelected).removeClass('hascontent');
 					menu.find('.' + settings.classMenuContent).css('visibility','hidden');
-			  },1200);
+			  },settings.hideMenuTiming);
 			});
 			settings.afterInit(menu, menu.callbackInit);                                                // call user function
 		};
 		
 		menu.itemSelect = function(){
+			var showTiming = settings.showContentTiming;
 			clearTimeout(menutimers.itemContentShow);
       if ($("html").hasClass("not-selectable")) return false;
+			menuCurrentItem.addClass(settings.classSelected);
+			menuCurrent = menuCurrentItem.parents('.' + settings.classMenuContent).eq(0);
+			if (menuCurrent.length < 1) { 
+			  menuCurrent = menu;
+				showTiming = 10;
+			}
+      menuCurrent.find('li').not(menuCurrentItem).removeClass(settings.classSelected).removeClass('hascontent');
       menuCurrentItemContent = menuCurrentItem.children('.' + settings.classMenuContent);
-			menutimers.itemContentShow = setTimeout(menu.showContent,settings.showContentTiming);
+			menutimers.itemContentShow = setTimeout(menu.showContent, showTiming);
 			settings.afterSelect(menuCurrentItem, menuCurrentItemContent, menu.callbackInit);           // call user function
 		};
 		
 		menu.showContent = function(){
 			settings.beforeShow(menuCurrentItem, menuCurrentItemContent, menu.callbackInit);            // call user function
-			menuCurrent = menuCurrentItem.parents('.' + settings.classMenuContent).eq(0);
-			if (menuCurrent.length < 1) menuCurrent = menu;
-      menuCurrent.find('li').removeClass(settings.classSelected).removeClass('hascontent');
-			menuCurrentItem.addClass(settings.classSelected);
 			menu.hideContent(menuCurrentItemContent);
 			if(menuCurrentItemContent.length > 0) {
 				if (menuCurrentItem.data('menutype') == 'horizontal') {
@@ -82,13 +87,13 @@
         menuCurrentItemContent.css('visibility','visible');
 				menuCurrentItem.addClass('hascontent');
 			}
-			settings.afterShow(menuCurrentItem, menuCurrentItemContent, menu.callbackInit);                             // call user function
+			settings.afterShow(menuCurrentItem, menuCurrentItemContent, menu.callbackInit);                                          // call user function
 		};
 		
 		menu.hideContent = function(menuCurrentItemContent){
-			settings.beforeHide(menuCurrentItem, menuCurrentItemContent, menu.callbackInit);                            // call user function
+			settings.beforeHide(menuCurrentItem, menuCurrentItemContent, menuCurrent, menu.callbackInit);                            // call user function
 			menuCurrent.find('.' + settings.classMenuContent).not(menuCurrentItemContent).css('visibility','hidden');
-			settings.afterHide(menuCurrentItem, menuCurrentItemContent, menu.callbackInit);                             // call user function
+			settings.afterHide(menuCurrentItem, menuCurrentItemContent, menuCurrent, menu.callbackInit);                             // call user function
 		};
 		
 		menu.callbackInit = function(callback, acallbackItem, acallbackItemContent){
